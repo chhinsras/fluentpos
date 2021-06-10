b4b528ec6178cd2470af9a4ea18a7179e93ced73
Mukesh Murugan <iammukeshm@gmail.com>
2021-06-10 08:34:12 +0530

    service refactor

--- src/Server/Modules/Identity/Modules.Identity.Core/Exceptions/IdentityException.cs
+++ src/Server/Modules/Identity/Modules.Identity.Core/Exceptions/IdentityException.cs
@@ -1,9 +1,4 @@
 ﻿using FluentPOS.Shared.Abstractions.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FluentPOS.Modules.Identity.Core.Exceptions
 {
