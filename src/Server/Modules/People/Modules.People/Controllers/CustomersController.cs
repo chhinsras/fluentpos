f5d791ba4127087984d03ca6ed48d1d50ce8859a
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-06-17 11:53:45 +0700

    Add Customer Commands Feature

--- src/Server/Modules/People/Modules.People/Controllers/CustomersController.cs
+++ src/Server/Modules/People/Modules.People/Controllers/CustomersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FluentPOS.Modules.People.Core.Features.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,23 @@ namespace FluentPOS.Modules.People.Controllers
 {
     internal class CustomersController : BaseController
     {
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterCustomerCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCustomerCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await Mediator.Send(new RemoveCustomerCommand(id)));
+        }
     }
 }
