5a0d38607b1521edb427f6822238560f274eb346
Mukesh Murugan <iammukeshm@gmail.com>
2021-06-20 18:17:02 +0530

    refactored type helpers

---
+++ src/server/Shared/Shared.Infrastructure/Utilities/TypeUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace FluentPOS.Shared.Infrastructure
+{
+    public static class TypeUtilities
+    {
+        public static List<T> GetAllPublicConstantValues<T>(this Type type)
+        {
+            return type
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(T))
+                .Select(x => (T)x.GetRawConstantValue())
+                .ToList();
+        }
+        public static List<string> GetNestedClassesStaticStringValues(this Type type)
+        {
+            var values = new List<string>();
+            foreach (var prop in type.GetNestedTypes().SelectMany(c => c.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)))
+            {
+                var propertyValue = prop.GetValue(null);
+                if (propertyValue is not null)
+                    values.Add(propertyValue.ToString());
+            }
+            return values;
+        }
+    }
+}
