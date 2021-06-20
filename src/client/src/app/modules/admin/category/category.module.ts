565fcafffe0b9662e339338dbfd98098952a63e6
Mukesh Murugan <iammukeshm@gmail.com>
2021-06-20 21:45:18 +0530

    modular folder structure - angular

---
+++ src/client/src/app/modules/admin/category/category.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CategoryRoutingModule } from './category-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CategoryRoutingModule
+  ]
+})
+export class CategoryModule { }
---
+++ src/client/src/app/modules/admin/supplier/supplier.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SupplierRoutingModule } from './supplier-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SupplierRoutingModule
+  ]
+})
+export class SupplierModule { }
