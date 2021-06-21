f7c4cf5f0527f6f6252a57976afff4dd2c8dd33b
Mukesh Murugan <iammukeshm@gmail.com>
2021-06-21 14:37:52 +0530

    400 error code fix

--- src/server/Shared/Shared.Core/Exceptions/CustomValidationException.cs
+++ src/server/Shared/Shared.Core/Exceptions/CustomValidationException.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 
 namespace FluentPOS.Shared.Core.Exceptions
 {
     public class CustomValidationException : CustomException
     {
-        public CustomValidationException(List<string> errors) : base("One or more validation failures have occurred.", errors)
+        public CustomValidationException(List<string> errors) : base("One or more validation failures have occurred.", errors,HttpStatusCode.BadRequest)
         {
         }
     }
