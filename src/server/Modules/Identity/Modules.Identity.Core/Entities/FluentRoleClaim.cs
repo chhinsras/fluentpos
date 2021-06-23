8cc165f1d3682e21548b99bc5754a270e464285c
Chebotov Nikolay <spiritkola@hotmail.com>
2021-06-23 18:01:06 +0300

    fix(FluentRoleClaim): Use FluentRoleClaim instead of IdentityUserRole<string>

--- src/server/Modules/Identity/Modules.Identity.Core/Entities/FluentRoleClaim.cs
+++ src/server/Modules/Identity/Modules.Identity.Core/Entities/FluentRoleClaim.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 
 namespace FluentPOS.Modules.Identity.Core.Entities
 {
--- src/server/Modules/Identity/Modules.Identity.Infrastructure/Persistence/Migrations/20210623145546_AddFluentRoleClaim.Designer.cs
+++ src/server/Modules/Identity/Modules.Identity.Infrastructure/Persistence/Migrations/20210623145546_AddFluentRoleClaim.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using FluentPOS.Modules.Identity.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace FluentPOS.Modules.Identity.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210623145546_AddFluentRoleClaim")]
+    partial class AddFluentRoleClaim
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
