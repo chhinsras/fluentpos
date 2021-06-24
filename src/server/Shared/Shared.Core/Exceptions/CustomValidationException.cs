16a1d9d5437b5643acda1685a003e50b48f6e0e2
Chebotov Nikolay <spiritkola@hotmail.com>
2021-06-24 22:26:27 +0300

    update(ValidationBehavior): Add IStringLocalizer

--- src/server/Shared/Shared.Core/Exceptions/CustomValidationException.cs
+++ src/server/Shared/Shared.Core/Exceptions/CustomValidationException.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Net;
+using Microsoft.Extensions.Localization;
 
 namespace FluentPOS.Shared.Core.Exceptions
 {
     public class CustomValidationException : CustomException
     {
-        public CustomValidationException(List<string> errors) : base("One or more validation failures have occurred.", errors, HttpStatusCode.BadRequest)
+        public CustomValidationException(IStringLocalizer localizer, List<string> errors) : base(localizer["One or more validation failures have occurred."], errors, HttpStatusCode.BadRequest)
         {
         }
     }
