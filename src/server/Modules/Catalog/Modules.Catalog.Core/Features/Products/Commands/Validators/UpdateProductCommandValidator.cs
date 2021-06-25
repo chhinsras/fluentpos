d5326f922bf1ebb749e6c31dbe39dec1c121f820
Chebotov Nikolay <spiritkola@hotmail.com>
2021-06-25 17:54:18 +0300

    chore: Add error message for Id property validation

--- src/server/Modules/Catalog/Modules.Catalog.Core/Features/Products/Commands/Validators/UpdateProductCommandValidator.cs
+++ src/server/Modules/Catalog/Modules.Catalog.Core/Features/Products/Commands/Validators/UpdateProductCommandValidator.cs
@@ -9,7 +9,7 @@ namespace FluentPOS.Modules.Catalog.Core.Features.Products.Commands.Validators
         public UpdateProductCommandValidator(IStringLocalizer<UpdateProductCommandValidator> localizer)
         {
             RuleFor(c => c.Id)
-               .NotEqual(Guid.Empty);
+               .NotEqual(Guid.Empty).WithMessage(x => localizer["The {PropertyName} property cannot be empty."]);
             RuleFor(c => c.Name)
                .NotEmpty().WithMessage(localizer["The {PropertyName} property cannot be empty."])
                .Length(2, 150).WithMessage(localizer["The {PropertyName} property must have between 2 and 150 characters."]);
