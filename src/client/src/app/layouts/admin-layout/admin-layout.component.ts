b2a6c3c4c87b1cae2320d4d871ff74f7ecb22ec1
Mukesh Murugan <iammukeshm@gmail.com>
2021-06-29 20:00:50 +0530

    added dark mode to dialogs

--- src/client/src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/client/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,4 +1,6 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
@@ -11,13 +13,19 @@ export class AdminLayoutComponent implements OnInit {
   title = 'angular-material-boilerplate';
   themeVariant: string = '';
   darkModeIcon: string = '';
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService,private dialog: MatDialog, private overlay: OverlayContainer) { }
   ngOnInit() {
     this.themeVariant = this.localStorageService.getItem('themeVariant');
   }
   onDarkModeToggled(data: { isDarkMode: boolean, darkModeIcon: string }) {
     console.log(data.isDarkMode);
+    
     this.themeVariant = data.isDarkMode ? 'dark-theme' : '';
+    if (data.isDarkMode) {
+      this.overlay.getContainerElement().classList.add('dark-theme');
+    } else {
+      this.overlay.getContainerElement().classList.remove('dark-theme');
+    }
     this.localStorageService.setItem('themeVariant', this.themeVariant);
     this.darkModeIcon = data.darkModeIcon;
   }
