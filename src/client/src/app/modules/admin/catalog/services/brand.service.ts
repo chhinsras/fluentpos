221775d54809f947725a26a254f1f5f5bdc0375f
Klevis Ndoka <klevisn@gmail.com>
2021-07-12 21:15:00 +0200

    Brands Service type definition & formatting

--- src/client/src/app/modules/admin/catalog/services/brand.service.ts
+++ src/client/src/app/modules/admin/catalog/services/brand.service.ts
@@ -1,12 +1,12 @@
-import { HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/internal/operators/map';
-import { IResult } from 'src/app/core/models/wrappers/IResult';
-import { PaginatedResult } from 'src/app/core/models/wrappers/PaginatedResult';
-import { BrandApiService } from '../api/brand-api.service';
-import { Brand } from '../models/brand';
-import { BrandParams } from '../models/brandParams';
+import {HttpParams} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/internal/operators/map';
+import {IResult} from 'src/app/core/models/wrappers/IResult';
+import {PaginatedResult} from 'src/app/core/models/wrappers/PaginatedResult';
+import {BrandApiService} from '../api/brand-api.service';
+import {Brand} from '../models/brand';
+import {BrandParams} from '../models/brandParams';
 
 @Injectable({
   providedIn: 'root'
@@ -15,36 +15,35 @@ export class BrandService {
   constructor(private api: BrandApiService) {
   }
 
-  getBrands(brandParams: BrandParams) {
+  getBrands(brandParams: BrandParams): Observable<PaginatedResult<Brand>> {
     let params = new HttpParams();
-    if (brandParams.searchString) params = params.append('searchString', brandParams.searchString)
-    if (brandParams.pageNumber) params = params.append('pageNumber', brandParams.pageNumber.toString())
+    if (brandParams.searchString) params = params.append('searchString', brandParams.searchString);
+    if (brandParams.pageNumber) params = params.append('pageNumber', brandParams.pageNumber.toString());
     if (brandParams.pageSize) params = params.append('pageSize', brandParams.pageSize.toString());
     if (brandParams.orderBy) params = params.append('orderBy', brandParams.orderBy.toString());
-    return this.api.getAlls(params).pipe(
-      map((response: PaginatedResult<Brand>) => response)
-    );
+    return this.api.getAlls(params)
+      .pipe(map((response: PaginatedResult<Brand>) => response));
   }
 
-  getBrandById(id: string) {
+  getBrandById(id: string): Observable<Brand> {
     return this.api.getById(id).pipe(
       map((response: Brand) => response)
     );
   }
 
-  createBrand(brand: Brand){
+  createBrand(brand: Brand): Observable<IResult<Brand>> {
     return this.api.create(brand).pipe(
       map((response: IResult<Brand>) => response)
     );
   }
 
-  updateBrand(brand: Brand) {
+  updateBrand(brand: Brand): Observable<IResult<Brand>> {
     return this.api.update(brand).pipe(
       map((response: IResult<Brand>) => response)
     );
   }
 
-  deleteBrand(id: string) : Observable<IResult<string>> {
+  deleteBrand(id: string): Observable<IResult<string>> {
     return this.api.delete(id).pipe(
       map((response: IResult<string>) => response)
     );
