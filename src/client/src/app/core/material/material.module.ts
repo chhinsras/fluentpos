97a38a2735a74bcef38959545caa6f375e63ffe5
Klevis Ndoka <klevisn@gmail.com>
2021-07-13 22:30:01 +0200

    Import mat checkbox module

--- src/client/src/app/core/material/material.module.ts
+++ src/client/src/app/core/material/material.module.ts
@@ -15,6 +15,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSortModule } from '@angular/material/sort';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 const Modules =
   [
@@ -33,7 +34,8 @@ const Modules =
     MatPaginatorModule,
     MatDialogModule,
     MatGridListModule,
-    MatSortModule
+    MatSortModule,
+    MatCheckboxModule
   ];
 
 @NgModule({
