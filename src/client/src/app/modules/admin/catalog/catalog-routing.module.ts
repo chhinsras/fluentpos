2626cc3033821405eb18b5b5040fc8deffdeccf6
Klevis Ndoka <klevisn@gmail.com>
2021-07-14 11:22:05 +0200

    LazyLoad Catalog Module

--- src/client/src/app/modules/admin/catalog/catalog-routing.module.ts
+++ src/client/src/app/modules/admin/catalog/catalog-routing.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CatalogComponent } from './catalog.component';
-import { BrandComponent } from './components/brand/brand.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BrandComponent} from './components/brand/brand.component';
+import {CategoryComponent} from './components/category/category.component';
+import {ProductComponent} from './components/product/product.component';
 
 const routes: Routes = [
+  {
+    path: 'brands',
+    component: BrandComponent
+  },
+  {
+    path: 'categories',
+    component: CategoryComponent
+  },
+  {
+    path: 'products',
+    component: ProductComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CatalogRoutingModule { }
+export class CatalogRoutingModule {
+}
