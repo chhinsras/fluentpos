65c1c97ef90b13be8cd7a47be95bb1a1259fbfb1
Mukesh Murugan <iammukeshm@gmail.com>
2021-07-15 00:45:23 +0530

    ui updates

--- src/client/src/app/modules/home/welcome/welcome.component.ts
+++ src/client/src/app/modules/home/welcome/welcome.component.ts
@@ -10,10 +10,14 @@ export class WelcomeComponent implements OnInit {
 
   fullName: string = '';
   email: string;
+  date:Date=new Date();
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
     this.getUserDetails();
+    setInterval(() => {
+      this.date = new Date()
+    }, 1000)
   }
 
   getUserDetails() {
