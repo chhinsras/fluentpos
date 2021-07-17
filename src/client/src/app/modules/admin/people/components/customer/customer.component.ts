9ccafe50e3d2491b1e9ec4b773ffdaf2a4c3cb8c
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-07-17 13:36:23 +0700

    Clean up customer table component

--- src/client/src/app/modules/admin/people/components/customer/customer.component.ts
+++ src/client/src/app/modules/admin/people/components/customer/customer.component.ts
@@ -1,97 +1,93 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {PaginatedFilter} from 'src/app/core/models/Filters/PaginatedFilter';
-import {PaginatedResult} from 'src/app/core/models/wrappers/PaginatedResult';
-import {DeleteDialogComponent} from '../../../shared/components/delete-dialog/delete-dialog.component';
-import {Customer} from '../../models/customer';
-import {CustomerParams} from '../../models/customerParams';
-import {CustomerService} from '../../services/customer.service';
-import {CustomerFormComponent} from './customer-form/customer-form.component';
-import {ToastrService} from 'ngx-toastr';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatSort, Sort} from '@angular/material/sort';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PaginatedFilter } from 'src/app/core/models/Filters/PaginatedFilter';
+import { PaginatedResult } from 'src/app/core/models/wrappers/PaginatedResult';
+import { Customer } from '../../models/customer';
+import { CustomerParams } from '../../models/customerParams';
+import { CustomerService } from '../../services/customer.service';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { ToastrService } from 'ngx-toastr';
+import { Sort } from '@angular/material/sort';
+import { TableColumn } from 'src/app/core/shared/components/table/table-column';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.scss'],
 })
-export class CustomerComponent implements OnInit, AfterViewInit {
-
+export class CustomerComponent implements OnInit {
   customers: PaginatedResult<Customer>;
-  customerColumns: string[] = ['id', 'name', 'phone', 'email',  'type', 'action'];
+  customerColumns: TableColumn[];
   customerParams = new CustomerParams();
-  dataSource = new MatTableDataSource<Customer>([]);
   searchString: string;
-  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public customerService: CustomerService, public dialog: MatDialog, public toastr: ToastrService) {
-  }
+  constructor(
+    public customerService: CustomerService,
+    public dialog: MatDialog,
+    public toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.getCustomers();
-  }
-
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
+    this.initColumns();
   }
 
   getCustomers(): void {
     this.customerService.getCustomers(this.customerParams).subscribe((result) => {
       this.customers = result;
-      this.dataSource.data = this.customers.data;
     });
   }
 
-  handlePageChange(event: PaginatedFilter): void {
+  initColumns(): void {
+    this.customerColumns = [
+      { name: 'Id', dataKey: 'id', isSortable: true, isShowable: true },
+      { name: 'Name', dataKey: 'name', isSortable: true, isShowable: true },
+      { name: 'Phone', dataKey: 'phone', isSortable: true, isShowable: true },
+      { name: 'Email', dataKey: 'email', isSortable: true, isShowable: true },
+      { name: 'Type', dataKey: 'type', isSortable: true, isShowable: true },
+      { name: 'Action', dataKey: 'action', position: 'right' },
+    ];
+  }
+
+  pageChanged(event: PaginatedFilter): void {
     this.customerParams.pageNumber = event.pageNumber;
     this.customerParams.pageSize = event.pageSize;
     this.getCustomers();
   }
 
-  openCustomerForm(customer?: Customer): void {
+  openForm(customer?: Customer): void {
     const dialogRef = this.dialog.open(CustomerFormComponent, {
-      data: customer
+      data: customer,
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.getCustomers();
       }
     });
   }
 
-  openDeleteConfirmationDialog(id: string): void {
-    const dialogRef = this.dialog.open(DeleteDialogComponent, {
-      data: 'Do you confirm the removal of this customer?'
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.removeCustomer(id);
-      }
-    });
-  }
-
-  removeCustomer(id: string): void {
-    this.customerService.deleteCustomer(id).subscribe(() => {
+  remove($event: string): void {
+    this.customerService.deleteCustomer($event).subscribe(() => {
       this.getCustomers();
       this.toastr.info('Customer Removed');
     });
   }
 
-  doSort(sort: Sort): void {
-    this.customerParams.orderBy = sort.active + ' ' + sort.direction;
+  sort($event: Sort): void {
+    this.customerParams.orderBy = $event.active + ' ' + $event.direction;
+    console.log(this.customerParams.orderBy);
     this.getCustomers();
   }
 
-  public doFilter(): void {
-    this.customerParams.searchString = this.searchString.trim().toLocaleLowerCase();
+  filter($event: string): void {
+    this.customerParams.searchString = $event.trim().toLocaleLowerCase();
     this.customerParams.pageNumber = 0;
     this.customerParams.pageSize = 0;
     this.getCustomers();
   }
 
-  public reload(): void {
-    this.searchString = this.customerParams.searchString = '';
+  reload(): void {
+    this.customerParams.searchString = '';
     this.customerParams.pageNumber = 0;
     this.customerParams.pageSize = 0;
     this.getCustomers();
