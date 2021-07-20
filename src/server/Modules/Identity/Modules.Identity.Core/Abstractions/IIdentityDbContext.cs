6de606272a337e6fec83942d98a8d8f354baae78
Chebotov Nikolay <spiritkola@hotmail.com>
2021-07-20 21:52:19 +0300

    update: Add IDbContext

--- src/server/Modules/Identity/Modules.Identity.Core/Abstractions/IIdentityDbContext.cs
+++ src/server/Modules/Identity/Modules.Identity.Core/Abstractions/IIdentityDbContext.cs
@@ -1,20 +1,15 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using FluentPOS.Modules.Identity.Core.Entities;
+﻿using FluentPOS.Modules.Identity.Core.Entities;
+using FluentPOS.Shared.Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace FluentPOS.Modules.Identity.Core.Abstractions
 {
-    public interface IIdentityDbContext
+    public interface IIdentityDbContext : IDbContext
     {
         public DbSet<FluentUser> Users { get; set; }
 
         public DbSet<FluentRole> Roles { get; set; }
 
         public DbSet<FluentRoleClaim> RoleClaims { get; set; }
-
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new());
-
-        int SaveChanges();
     }
 }
