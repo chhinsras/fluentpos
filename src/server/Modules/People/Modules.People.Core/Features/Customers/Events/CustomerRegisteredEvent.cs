095493ee061f2fe83e38df9817e582bb93fbc6d7
Chebotov Nikolay <spiritkola@hotmail.com>
2021-07-21 22:57:48 +0300

    update(Events): Pass entity to Customer events constructor

--- src/server/Modules/People/Modules.People.Core/Features/Customers/Events/CustomerRegisteredEvent.cs
+++ src/server/Modules/People/Modules.People.Core/Features/Customers/Events/CustomerRegisteredEvent.cs
@@ -1,26 +1,27 @@
 ﻿using FluentPOS.Shared.Core.Domain;
 using System;
+using FluentPOS.Modules.People.Core.Entities;
 
 namespace FluentPOS.Modules.People.Core.Features.Customers.Events
 {
     public class CustomerRegisteredEvent : Event
     {
-        public CustomerRegisteredEvent(Guid id, string name, string phone, string email, string imageUrl, string type)
-        {
-            Name = name;
-            Phone = phone;
-            Email = email;
-            ImageUrl = imageUrl;
-            Type = type;
-            Id = id;
-            AggregateId = id;
-        }
-
         public Guid Id { get; }
         public string Name { get; }
         public string Phone { get; }
         public string Email { get; }
         public string ImageUrl { get; }
         public string Type { get; }
+
+        public CustomerRegisteredEvent(Customer customer)
+        {
+            Name = customer.Name;
+            Phone = customer.Phone;
+            Email = customer.Email;
+            ImageUrl = customer.ImageUrl;
+            Type = customer.Type;
+            Id = customer.Id;
+            AggregateId = customer.Id;
+        }
     }
 }
