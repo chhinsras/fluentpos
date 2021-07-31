028ff09c36e1949a23d02a66591a863c0e93d267
Chebotov Nikolay <spiritkola@hotmail.com>
2021-07-31 18:31:40 +0300

    update: Add ValidatorInterceptor

---
+++ src/server/Shared/Shared.Infrastructure/Interceptors/ValidatorInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using FluentPOS.Shared.Core.Exceptions;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace FluentPOS.Shared.Infrastructure.Interceptors
+{
+    public class ValidatorInterceptor : IValidatorInterceptor
+    {
+        private readonly IStringLocalizer<ValidatorInterceptor> _localizer;
+
+        public ValidatorInterceptor(IStringLocalizer<ValidatorInterceptor> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public IValidationContext BeforeAspNetValidation(ActionContext actionContext, IValidationContext commonContext)
+        {
+            return commonContext;
+        }
+
+        public ValidationResult AfterAspNetValidation(ActionContext actionContext, IValidationContext validationContext, ValidationResult result)
+        {
+            var failures = result.Errors.Where(f => f != null).ToList();
+
+            if (failures.Count != 0)
+            {
+                var errorMessages = failures.Select(a => a.ErrorMessage).Distinct().ToList();
+                throw new CustomValidationException(_localizer, errorMessages);
+            }
+
+            return result;
+        }
+    }
+}
