40b08a97aba9f59a38ef33d36db73e8c9e206d97
Chebotov Nikolay <spiritkola@hotmail.com>
2021-07-31 18:43:31 +0300

    update(People): Add customer filter validators
    docs: Update adding-extended-attribute-tutorial.md

---
+++ src/server/Modules/People/Modules.People.Core/Features/ExtendedAttributes/Validators/Customers/CustomerPaginatedExtendedAttributeFilterValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentPOS.Modules.People.Core.Entities;
+using FluentPOS.Shared.Core.Features.ExtendedAttributes.Queries.Validators;
+using Microsoft.Extensions.Localization;
+
+namespace FluentPOS.Modules.People.Core.Features.ExtendedAttributes.Validators.Customers
+{
+    public class CustomerPaginatedExtendedAttributeFilterValidator : PaginatedExtendedAttributeFilterValidator<Guid, Customer>
+    {
+        public CustomerPaginatedExtendedAttributeFilterValidator(IStringLocalizer<CustomerPaginatedExtendedAttributeFilterValidator> localizer) : base(localizer)
+        {
+            // you can override the validation rules here
+        }
+    }
+}
