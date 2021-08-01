97206fcddff45e0985f6351426ec67f576b2daed
Chebotov Nikolay <spiritkola@hotmail.com>
2021-08-01 12:30:12 +0300

    update(API versioning): Add CommonBaseController

---
+++ src/server/Shared/Shared.Infrastructure/Controllers/CommonBaseController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentPOS.Shared.Infrastructure.Controllers
+{
+    [ApiController]
+    [Route(BasePath + "/[controller]")]
+    public abstract class CommonBaseController : ControllerBase
+    {
+        protected internal const string BasePath = "api/v{version:apiVersion}";
+
+        private IMediator _mediatorInstance;
+        protected IMediator Mediator => _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        private IMapper _mapperInstance;
+        protected IMapper Mapper => _mapperInstance ??= HttpContext.RequestServices.GetService<IMapper>();
+    }
+}
