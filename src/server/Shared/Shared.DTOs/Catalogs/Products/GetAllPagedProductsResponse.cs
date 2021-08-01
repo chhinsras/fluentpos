069d373bfdf55452df3a1fef28c0729cecc83c3b
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-08-01 14:08:21 +0700

    Return BrandName and CategoryName

--- src/server/Shared/Shared.DTOs/Catalogs/Products/GetAllPagedProductsResponse.cs
+++ src/server/Shared/Shared.DTOs/Catalogs/Products/GetAllPagedProductsResponse.cs
@@ -2,5 +2,5 @@
 
 namespace FluentPOS.Shared.DTOs.Catalogs.Products
 {
-    public record GetAllPagedProductsResponse(Guid Id, string Name, string LocaleName, string BarcodeSymbology, string Detail, Guid BrandId, Guid CategoryId, decimal Price, decimal Cost, string ImageUrl, string Tax, string TaxMethod, bool IsAlert, decimal AlertQuantity);
+    public record GetAllPagedProductsResponse(Guid Id, string Name, string LocaleName, string BarcodeSymbology, string Detail, string BrandName, string CategoryName, decimal Price, decimal Cost, string ImageUrl, string Tax, string TaxMethod, bool IsAlert, decimal AlertQuantity);
 }
