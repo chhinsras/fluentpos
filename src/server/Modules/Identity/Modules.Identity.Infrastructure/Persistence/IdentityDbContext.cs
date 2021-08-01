d861a824e52a27c1021ddba74afa2f86a780342d
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-01 15:22:28 +0530

    re-add tracking to indentity context

--- src/server/Modules/Identity/Modules.Identity.Infrastructure/Persistence/IdentityDbContext.cs
+++ src/server/Modules/Identity/Modules.Identity.Infrastructure/Persistence/IdentityDbContext.cs
@@ -51,7 +51,56 @@ namespace FluentPOS.Modules.Identity.Infrastructure.Persistence
             modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
             modelBuilder.ApplyIdentityConfiguration(_persistenceOptions);
         }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var changes = OnBeforeSaveChanges();
+            return await this.SaveChangeWithPublishEventsAsync(_eventLogger, _mediator, changes, cancellationToken);
+        }
+        private (string oldValues, string newValues) OnBeforeSaveChanges()
+        {
+            var previousData = new Dictionary<string, object>();
+            var currentData = new Dictionary<string, object>();
+            ChangeTracker.DetectChanges();
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                foreach (var property in entry.Properties)
+                {
+                    string propertyName = property.Metadata.Name;
+                    var originalValue = entry.GetDatabaseValues()?.GetValue<object>(propertyName);
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            currentData[propertyName] = property.CurrentValue;
+                            break;
+                        case EntityState.Deleted:
+                            previousData[propertyName] = originalValue;
+                            break;
 
+                        case EntityState.Modified:
+
+                            if (property.IsModified && originalValue?.Equals(property.CurrentValue) == false)
+                            {
+                                previousData[propertyName] = originalValue;
+                                currentData[propertyName] = property.CurrentValue;
+                            }
+                            break;
+                    }
+                }
+            }
+            var oldValues = previousData.Count == 0 ? null : JsonConvert.SerializeObject(previousData);
+            var newValues = currentData.Count == 0 ? null : JsonConvert.SerializeObject(currentData);
+            return (oldValues: oldValues, newValues: newValues);
+        }
+        public override int SaveChanges()
+        {
+            var changes = OnBeforeSaveChanges();
+            return this.SaveChangeWithPublishEvents(_eventLogger, _mediator, changes);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            return SaveChanges();
+        }
         DbSet<FluentUser> IExtendedAttributeDbContext<string, FluentUser, UserExtendedAttribute>.GetEntities() => Users;
 
         DbSet<UserExtendedAttribute> IExtendedAttributeDbContext<string, FluentUser, UserExtendedAttribute>.ExtendedAttributes { get; set; }
