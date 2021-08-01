a2852f0af04613bcb413219d91738c3197745dca
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-01 12:50:39 +0530

    added log component - ng

---
+++ src/client/src/app/modules/admin/activity-logs/activity-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-logs',
+  templateUrl: './activity-logs.component.html',
+  styleUrls: ['./activity-logs.component.scss']
+})
+export class ActivityLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
