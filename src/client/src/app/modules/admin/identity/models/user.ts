2a52b8c86c8f750b69e91c69f452b4809e1f494a
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-08-03 14:29:56 +0700

    Add User and UserParams Model

---
+++ src/client/src/app/modules/admin/identity/models/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: string
+    userName: string
+    firstName: string
+    lastName: string
+    email: string
+    isActive: boolean
+    emailConfirmed: boolean
+    phoneNumber: string
+    profilePictureUrl: string
+}
+  
