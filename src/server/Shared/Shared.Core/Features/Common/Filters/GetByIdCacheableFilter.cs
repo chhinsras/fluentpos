db1fd4bd74a0748ae7c66893819cfbdcbcb10a46
Chebotov Nikolay <spiritkola@hotmail.com>
2021-08-04 18:18:04 +0300

    feat: Add GetByIdCacheableFilter

---
+++ src/server/Shared/Shared.Core/Features/Common/Filters/GetByIdCacheableFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using FluentPOS.Shared.Core.Attributes;
+using FluentPOS.Shared.Core.Constants;
+using FluentPOS.Shared.Core.Contracts;
+using FluentPOS.Shared.Core.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentPOS.Shared.Core.Features.Common.Filters
+{
+    public class GetByIdCacheableFilter<TEntityId, TEntity> : ICacheable
+        where TEntity : class, IEntity<TEntityId>
+    {
+        [FromRoute(Name = "id")]
+        public TEntityId Id { get; set; }
+
+        [FromQuery]
+        public bool BypassCache { get; set; }
+
+        [FromQuery]
+        [SwaggerExclude]
+        public string CacheKey => CacheKeys.Common.GetEntityByIdCacheKey<TEntityId, TEntity>(Id);
+
+        [FromQuery]
+        [RegularExpression(@"^([0-9]{1}|(?:0[0-9]|1[0-9]|2[0-3])+):([0-5]?[0-9])(?::([0-5]?[0-9])(?:.(\d{1,9}))?)?$")]
+        public TimeSpan? SlidingExpiration { get; set; }
+
+        public GetByIdCacheableFilter()
+        {
+        }
+    }
+}
