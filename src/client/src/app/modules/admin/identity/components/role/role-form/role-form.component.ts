88c7d1b2c5e59e70d6c492d942a2f89d882811d5
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-08-05 15:08:12 +0700

    Add Role Form

--- src/client/src/app/modules/admin/identity/components/role/role-form/role-form.component.ts
+++ src/client/src/app/modules/admin/identity/components/role/role-form/role-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { Role } from '../../../models/role';
+import { RoleService } from '../../../services/role.service';
 
 @Component({
   selector: 'app-role-form',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role-form.component.scss']
 })
 export class RoleFormComponent implements OnInit {
-
-  constructor() { }
+  roleForm: FormGroup;
+  formTitle: string;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Role, private roleService: RoleService, private toastr: ToastrService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.roleForm = this.fb.group({
+      id: [this.data && this.data.id],
+      name: [this.data && this.data.name, Validators.required],
+      description: [this.data && this.data.description, Validators.required]
+    })
+    if (this.roleForm.get('id').value === "" || this.roleForm.get('id').value == null) {
+      this.formTitle = "Register Role";
+    }
+    else {
+      this.formTitle = "Edit Role";
+    }
+  }
+
+  onSubmit() {
+    if (this.roleForm.valid) {
+      if (this.roleForm.get('id').value === "" || this.roleForm.get('id').value == null) {
+        this.roleService.createRole(this.roleForm.value).subscribe(response => {
+          this.toastr.success(response.messages[0]);
+        })
+      } else {
+        this.roleService.updateRole(this.roleForm.value).subscribe(response => {
+          this.toastr.success(response.messages[0]);
+        })
+      }
+    }
   }
 
 }
