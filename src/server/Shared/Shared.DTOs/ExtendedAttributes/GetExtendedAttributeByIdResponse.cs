481644f2f9594e0c9cd902cbda5f8376721343cc
Chebotov Nikolay <spiritkola@hotmail.com>
2021-08-08 01:03:00 +0300

    chore: Add small fix

--- src/server/Shared/Shared.DTOs/ExtendedAttributes/GetExtendedAttributeByIdResponse.cs
+++ src/server/Shared/Shared.DTOs/ExtendedAttributes/GetExtendedAttributeByIdResponse.cs
@@ -12,5 +12,19 @@ using System;
 
 namespace FluentPOS.Shared.DTOs.ExtendedAttributes
 {
-    public record GetExtendedAttributeByIdResponse<TEntityId>(Guid Id, TEntityId EntityId, ExtendedAttributeType Type, string Key, decimal? Decimal, string? Text, DateTime? DateTime, string? Json, bool? Boolean, int? Integer, string? ExternalId, string? Group, string? Description, bool IsActive);
+    public record GetExtendedAttributeByIdResponse<TEntityId>(
+        Guid Id,
+        TEntityId EntityId,
+        ExtendedAttributeType Type,
+        string Key,
+        decimal? Decimal,
+        string? Text,
+        DateTime? DateTime,
+        string? Json,
+        bool? Boolean,
+        int? Integer,
+        string? ExternalId,
+        string? Group,
+        string? Description,
+        bool IsActive);
 }
