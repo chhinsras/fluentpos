761255b10cb53bd296e085ddf17c39924bb0b68b
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-07 17:31:03 +0530

    added event log details #82

---
+++ src/client/src/app/modules/admin/event-logs/components/event-log-details/event-log-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { EventLog } from 'src/app/core/models/event-logs/event-log';
+
+@Component({
+  selector: 'app-event-log-details',
+  templateUrl: './event-log-details.component.html',
+  styleUrls: ['./event-log-details.component.scss']
+})
+export class EventLogDetailsComponent implements OnInit {
+
+  brandForm: FormGroup;
+  formTitle: string = "Event Log Details";
+  constructor(@Inject(MAT_DIALOG_DATA) public data: EventLog, private toastr: ToastrService, private fb: FormBuilder) { }
+  jsonData;
+  jsonOldValues;
+  jsonNewValues;
+
+  ngOnInit(): void {
+    this.jsonData = this.parseToJson(this.data.data);
+    this.jsonNewValues = this.parseToJson(this.data.newValues);
+    this.jsonOldValues = this.parseToJson(this.jsonOldValues);
+  }
+  parseToJson(str: string) {
+    var json = JSON.parse(str);
+    return json;
+  }
+}
--- src/client/src/app/modules/admin/event-logs/event-logs.module.ts
+++ src/client/src/app/modules/admin/event-logs/event-logs.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventLogDetailsComponent } from './components/event-log-details/event-log-details.component';
+import { MaterialModule } from 'src/app/core/material/material.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EventLogDetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class ActivityLogsModule { }
