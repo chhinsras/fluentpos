aedf8ed479e2cb2b7d543727a0641e28f52c9a70
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-08 19:58:18 +0530

    fixes

--- src/server/Shared/Shared.Infrastructure/Persistence/Migrations/20210808141852_DescriptionFix.cs
+++ src/server/Shared/Shared.Infrastructure/Persistence/Migrations/20210808141852_DescriptionFix.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿// <copyright file="20210808141852_DescriptionFix.cs" company="Fluentpos">
+// --------------------------------------------------------------------------------------------------
+// Copyright (c) Fluentpos. All rights reserved.
+// The core team: Mukesh Murugan (iammukeshm), Chhin Sras (chhinsras), Nikolay Chebotov (unchase).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// --------------------------------------------------------------------------------------------------
+// </copyright>
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace FluentPOS.Shared.Infrastructure.Persistence.Migrations
 {
@@ -6,12 +14,10 @@ namespace FluentPOS.Shared.Infrastructure.Persistence.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
         }
     }
 }
