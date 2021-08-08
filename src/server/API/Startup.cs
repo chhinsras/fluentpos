fe2db56cc8a95f657b89259451b368e64c5da734
Chebotov Nikolay <spiritkola@hotmail.com>
2021-08-08 11:00:55 +0300

    chore: API code cleanup

--- src/server/API/Startup.cs
+++ src/server/API/Startup.cs
@@ -1,3 +1,11 @@
+// <copyright file="Startup.cs" company="Fluentpos">
+// --------------------------------------------------------------------------------------------------
+// Copyright (c) Fluentpos. All rights reserved.
+// The core team: Mukesh Murugan (iammukeshm), Chhin Sras (chhinsras), Nikolay Chebotov (unchase).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// --------------------------------------------------------------------------------------------------
+// </copyright>
+
 using FluentPOS.Modules.Catalog.Extensions;
 using FluentPOS.Modules.Identity.Extensions;
 using FluentPOS.Modules.People.Extensions;
