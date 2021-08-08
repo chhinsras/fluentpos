64c68ace5da9dcace7785afc60850c1d36c8ec63
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-08 10:34:05 +0530

    added sales.product

--- src/server/Modules/Sales/Modules.Sales.Core/Entities/Product.cs
+++ src/server/Modules/Sales/Modules.Sales.Core/Entities/Product.cs
@@ -1,7 +1,18 @@
+using System;
+
 namespace FluentPOS.Modules.Sales.Core.Entities
 {
     public class Product
     {
-        
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total { get; private set; }
+
     }
 }
