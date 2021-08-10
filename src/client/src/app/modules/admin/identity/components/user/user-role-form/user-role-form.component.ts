e6392723867df80946f82901ffaad0cd20e748c6
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-08-10 12:09:34 +0700

    Add UserRoleForm Component

---
+++ src/client/src/app/modules/admin/identity/components/user/user-role-form/user-role-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-role-form',
+  templateUrl: './user-role-form.component.html',
+  styleUrls: ['./user-role-form.component.scss']
+})
+export class UserRoleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
