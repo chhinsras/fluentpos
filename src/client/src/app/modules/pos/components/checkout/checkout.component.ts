2f6cc6819e99dff877ec3abbf17666d0a2e8c5c7
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-13 21:22:05 +0530

    checkout ui base

--- src/client/src/app/modules/pos/components/checkout/checkout.component.ts
+++ src/client/src/app/modules/pos/components/checkout/checkout.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Customer } from 'src/app/modules/admin/people/models/customer';
+import { CustomerService } from 'src/app/modules/admin/people/services/customer.service';
 import { CartItem } from '../../models/cart';
 import { CheckOut } from '../../models/checkOut';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,12 +13,14 @@ import { CheckOut } from '../../models/checkOut';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: CheckOut) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: CheckOut, public customerService:CustomerService, public cartService:CartService) { }
   cartItems: CartItem[];
   cartId: string;
+  customer: Customer;
   ngOnInit(): void {
     this.cartId = this.data.cartId;
     this.cartItems = this.data.cartItems;
+    this.customer = this.cartService.currentCustomer;
   }
 
 }
