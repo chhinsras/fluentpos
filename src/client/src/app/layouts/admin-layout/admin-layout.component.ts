69cd2a438f2aed69c141b77c13f9ad9a75a8d93d
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-08-17 14:39:04 +0700

    NG: Fixed ChangeDetectionStrategy

--- src/client/src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/client/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from 'src/app/core/services/auth.service';
@@ -6,6 +7,7 @@ import { BusyService } from 'src/app/core/services/busy.service';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
