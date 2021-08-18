b340f08f25145d198413caf9ee22894b152e06c7
Chebotov Nikolay <spiritkola@hotmail.com>
2021-08-18 22:10:33 +0300

    chore: Refactor StockService

--- src/server/Modules/Inventory/Modules.Inventory.Infrastructure/Services/StockService.cs
+++ src/server/Modules/Inventory/Modules.Inventory.Infrastructure/Services/StockService.cs
@@ -8,46 +8,30 @@
 
 using System;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using FluentPOS.Modules.Inventory.Core.Abstractions;
 using FluentPOS.Modules.Inventory.Core.Entities;
 using FluentPOS.Modules.Inventory.Core.Enums;
-using FluentPOS.Modules.Inventory.Core.Exceptions;
 using FluentPOS.Shared.Core.IntegrationServices.Inventory;
-using Microsoft.Extensions.Localization;
 
 namespace FluentPOS.Modules.Inventory.Infrastructure.Services
 {
-    /// <summary>
-    /// Integration Services for Inventory Module.
-    /// </summary>
+    /// <inheritdoc/>
     public class StockService : IStockService
     {
         private readonly IInventoryDbContext _context;
-        private readonly IStringLocalizer<StockService> _localizer;
 
         /// <summary>
         /// Stock Service.
         /// </summary>
         /// <param name="context">Context.</param>
-        /// <param name="localizer">Localizer.</param>
         public StockService(
-            IInventoryDbContext context,
-            IStringLocalizer<StockService> localizer)
+            IInventoryDbContext context)
         {
             _context = context;
-            _localizer = localizer;
         }
 
-        /// <summary>
-        /// Record Transaction.
-        /// </summary>
-        /// <param name="productId">Product Id.</param>
-        /// <param name="quantity">Quantity.</param>
-        /// <param name="referenceNumber">Reference Number.</param>
-        /// <param name="isSale">Is Sale.</param>
-        /// <returns>Task Completed.</returns>
+        /// <inheritdoc/>
         public async Task RecordTransaction(Guid productId, decimal quantity, string referenceNumber, bool isSale = true)
         {
             // TODO - Move this to MediatR, maybe? - Important, DO NOT make an API endpoint for this.
@@ -56,45 +40,40 @@ namespace FluentPOS.Modules.Inventory.Infrastructure.Services
             var stockTransaction = new StockTransaction(productId, quantity, transactionType, referenceNumber);
             await _context.StockTransactions.AddAsync(stockTransaction);
 
-            bool hasStockRecord = _context.Stocks.Any(a => a.ProductId == productId);
-            if (hasStockRecord)
+            var stockRecord = _context.Stocks.FirstOrDefault(s => s.ProductId == productId);
+            switch (transactionType)
             {
-                if (isSale)
-                {
-                    var stockRecord = _context.Stocks.FirstOrDefault(s => s.ProductId == productId);
-                    if (stockRecord == null)
+                case TransactionType.Sale:
+                    if (stockRecord != null)
                     {
-                        throw new InventoryException(_localizer["Stock Record Not Found"], HttpStatusCode.NotFound);
+                        stockRecord.ReduceQuantity(quantity);
+                        _context.Stocks.Update(stockRecord);
+                    }
+                    else
+                    {
+                        var newStockRecord = new Stock(productId);
+                        newStockRecord.ReduceQuantity(quantity);
+                        _context.Stocks.Add(newStockRecord);
                     }
 
-                    stockRecord.ReduceQuantity(quantity);
-                    _context.Stocks.Update(stockRecord);
-                }
-                else
-                {
-                    var stockRecord = _context.Stocks.FirstOrDefault(s => s.ProductId == productId);
-                    if (stockRecord == null)
+                    break;
+                case TransactionType.Purchase:
+                    if (stockRecord != null)
                     {
-                        throw new InventoryException(_localizer["Stock Record Not Found"], HttpStatusCode.NotFound);
+                        stockRecord.IncreaseQuantity(quantity);
+                        _context.Stocks.Update(stockRecord);
+                    }
+                    else
+                    {
+                        var newStockRecord = new Stock(productId);
+                        newStockRecord.IncreaseQuantity(quantity);
+                        _context.Stocks.Add(newStockRecord);
                     }
 
-                    stockRecord.IncreaseQuantity(quantity);
-                    _context.Stocks.Update(stockRecord);
-                }
-            }
-            else
-            {
-                var stockRecord = new Stock(productId);
-                if (isSale)
-                {
-                    stockRecord.ReduceQuantity(quantity);
-                }
-                else
-                {
-                    stockRecord.IncreaseQuantity(quantity);
-                }
+                    break;
 
-                _context.Stocks.Add(stockRecord);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(transactionType), transactionType, null);
             }
 
             await _context.SaveChangesAsync();
--- src/server/Shared/Shared.Core/IntegrationServices/Inventory/IStockService.cs
+++ src/server/Shared/Shared.Core/IntegrationServices/Inventory/IStockService.cs
@@ -11,8 +11,19 @@ using System.Threading.Tasks;
 
 namespace FluentPOS.Shared.Core.IntegrationServices.Inventory
 {
+    /// <summary>
+    /// Integration Services for Inventory Module.
+    /// </summary>
     public interface IStockService
     {
+        /// <summary>
+        /// Record Transaction.
+        /// </summary>
+        /// <param name="productId">Product Id.</param>
+        /// <param name="quantity">Quantity.</param>
+        /// <param name="referenceNumber">Reference Number.</param>
+        /// <param name="isSale">Is Sale.</param>
+        /// <returns>Task Completed.</returns>
         public Task RecordTransaction(Guid productId, decimal quantity, string referenceNumber, bool isSale = true);
     }
 }
