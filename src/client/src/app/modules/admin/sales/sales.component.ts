6dcb87194ea906afe06b988992fa1cd65545730f
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2021-09-29 15:11:12 +0700

    NG: Added Sales Module

---
+++ src/client/src/app/modules/admin/sales/sales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  templateUrl: './sales.component.html',
+  styleUrls: ['./sales.component.scss']
+})
+export class SalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/client/src/app/modules/admin/sales/sales.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalesComponent } from './sales.component';
+
+
+
+@NgModule({
+  declarations: [
+    SalesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SalesModule { }
